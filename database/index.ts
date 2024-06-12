8d3d49bd2a30b87355a1cb1734ecd12ba99e09d5
rizkanxcode <riezkanaprianda@gmail.com>
2024-06-13 00:20:18 +0700

    changes db config

--- database/index.ts
+++ database/index.ts
@@ -10,13 +10,13 @@ export default async function setupDatabase() {
     return DB;
   }
 
-  const host = process.env.DB_HOST ?? "";
-  const user = process.env.DB_USER ?? "";
-  const password = process.env.DB_PASSWORD ?? "";
-  const db = process.env.DB_NAME ?? "";
+  const host = process.env.DB_HOST ?? "0.0.0.0";
+  const user = process.env.DB_USER ?? "postgres";
+  const password = process.env.DB_PASSWORD ?? "password";
+  const db = process.env.DB_NAME ?? "fastify";
   const port = process.env.DB_PORT ?? 5432;
 
-  const Sql = postgres(process.env.DB_URL ?? "postgres://postgres:password@0.0.0.0:5432/fastify", { max: 1 });
+  const Sql = postgres(process.env.DB_URL ?? `postgres://${user}:${password}@${host}:${port}/${db}`, { max: 1 });
   try {
     DB = drizzle(Sql);
     // Optionally run migrations
