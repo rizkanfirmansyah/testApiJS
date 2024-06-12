29b887a497f640633c6817746069214f20defa06
rizkanxcode <riezkanaprianda@gmail.com>
2024-06-12 22:53:33 +0700

    create model auth register

--- src/modules/user/userModel.ts
+++ src/modules/user/userModel.ts
@@ -1,10 +1,23 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import { user } from "../../../database/schema/user";
 import setupDatabase from "../../../database";
+import { user } from "../../../database/schema/user";
+import { LoginType } from "../../utils/types/auth";
 
 export async function getUser() {
-  const DB = await setupDatabase(); // Await the setupDatabase function to get the actual Sql<{}> instance
+  const DB = await setupDatabase();
   const result = await DB.select().from(user);
 
   return result;
+}
+
+export async function registerUser({ username, password }: LoginType) {
+  const DB = await setupDatabase();
+  try {
+    await DB.insert(user).values({
+      username,
+      password,
+    });
+    return { message: "User registered successfully", status: 200 };
+  } catch (error) {
+    return { message: "User registered failed", status: 500, error };
+  }
 }
---
+++ src/utils/types/auth.ts
@@ -0,0 +1,11 @@
+interface RegisterType {
+  username: string;
+  password: string;
+}
+
+interface LoginType {
+  username: string;
+  password: string;
+}
+
+export { RegisterType, LoginType };
