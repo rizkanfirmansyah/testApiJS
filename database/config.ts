9c7673e7a2a6fd68809b435d0d67099441cd6bbd
rizkanxcode <riezkanaprianda@gmail.com>
2024-06-13 02:51:09 +0700

    update env config db

--- database/config.ts
+++ database/config.ts
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 export const dbhost = process.env.DB_HOST ?? "0.0.0.0";
 export const dbuser = process.env.DB_USER ?? "postgres";
 export const dbpassword = process.env.DB_PASSWORD ?? "password";
