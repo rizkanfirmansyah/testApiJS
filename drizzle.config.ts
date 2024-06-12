22aca9669c5dc93ad5a01e3dfb18af12bb217db0
rizkanxcode <riezkanaprianda@gmail.com>
2024-06-13 02:52:50 +0700

    fix bug config ts db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "drizzle-kit";
+import { dbname, dbhost, dbpassword, dbport, dbuser } from "./database/config";
 export default defineConfig({
   schema: "./database/schema/*",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DB_URL ?? "postgres://postgres:password@0.0.0.0:5432/fastify",
+    url: process.env.DB_URL ?? `postgres://${dbuser}:${dbpassword}@${dbhost}:${dbport}/${dbname}`,
   },
   verbose: true,
   strict: true,
