00ba0fe474cf7dc344e343475c83247e5b3993c2
rizkanxcode <riezkanaprianda@gmail.com>
2024-06-18 13:41:44 +0700

    update

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,22 @@
-import {buildServer} from "./server";
+import { buildServer } from "./server";
 
 const server = buildServer();
 
 async function main() {
   try {
-    await server.listen(3000, "0.0.0.0");
-
-    console.log(`Server ready at http://localhost:3000`);
+    server.listen(
+      {
+        port: 3000,
+        host: "0.0.0.0",
+      },
+      (err, address) => {
+        if (err) {
+          server.log.error(err);
+          process.exit(1);
+        }
+        server.log.info(`Server listening at ${address}`);
+      }
+    );
   } catch (e) {
     console.error(e);
     process.exit(1);
